720893eb461e6c428baeafa88024c53a48df2a1e
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-05-12 19:51:15 -0400

    better fizzbuzz

--- fizzbuzz/scripts.js
+++ fizzbuzz/scripts.js
@@ -1,9 +1,8 @@
-for (var i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0)
-    console.log("FizzBuzz");
-    else if (i % 3 === 0)
-    console.log("Fizz");
-    else if (i % 5 === 0)
-    console.log("Buzz");
-    else console.log(i);
+function fizzBuzz(inputNumber) {
+    for (let i = 1; i <= inputNumber; i++) {
+        if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+        else if (i % 3 === 0) console.log("Fizz");
+        else if (i % 5 === 0) console.log("Buzz");
+        else console.log(i);
+    }
 }
